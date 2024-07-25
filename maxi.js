bb7679b0a652fc0345832b50157486bbe044e58f
ojasvikachhwal <ojasvivok@gmail.com>
2024-07-25 12:06:51 +0530

    first commit

---
+++ maxi.js
@@ -0,0 +1,16 @@
+let a= 40;
+let b= 80;
+let max;
+if(a>=b){
+    if(a==b){
+        console.log("a and b are equal");}
+    else{
+        console.log(`max = ${a}`);}  } 
+else{
+    console.log(`max = ${b}`);
+} 
+
+let c=40;
+let d= 50;
+let maximum;
+console.log(a>b?`maximum=${c}`:`maximum=${d}`);
---
+++ swapp.js
@@ -0,0 +1,7 @@
+let a=20;
+let b=10;
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(`the new value of a: ${a}`);
+console.log(`the new value of b:${b}`);
